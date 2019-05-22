44177124cdbf4df15b2bf59675ee1c92ae5c8db1
김도현 <39308953+wookoo@users.noreply.github.com>
2019-05-22 14:40:20 +0900

    다항식의 덧셈 구현

---
+++ 6. 연결리스트-1/4. 연결리스트 - 다항식 덧셈.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+struct linkedNum {
+	int coef; //계수
+	int expon; //지수
+	struct linkedNum *next;
+};
+
+typedef struct linkedNum ListNode;
+
+int is_empty(ListNode *node) {
+	return node == NULL;
+}
+
+
+ListNode* insert_last(ListNode *head, element coef,element expon) {
+	ListNode *last = head;
+	while (last!= NULL && last->next != NULL) {
+		last = last->next;
+	}
+	ListNode *Node = (ListNode *)malloc(sizeof(ListNode));
+	Node->next = NULL;
+	Node->coef = coef;
+	Node->expon = expon;
+	
+	if (head == NULL) {
+		return Node;
+	}
+	last->next = Node;
+	return head;
+}
+ListNode* AddPoly(ListNode *first, ListNode *second) {
+	ListNode *firstTemp, *secondTemp;
+	firstTemp = first;
+	secondTemp = second;
+	ListNode *result = NULL;
+
+	while (firstTemp != NULL && secondTemp != NULL) {
+		if (firstTemp->expon == secondTemp->expon) {
+			//덧셈 연산
+			result = insert_last(result, firstTemp->coef + secondTemp->coef, firstTemp->expon);
+
+			firstTemp = firstTemp->next;
+			secondTemp = secondTemp->next;
+		
+
+		}
+		else if (firstTemp->expon > secondTemp->expon) {
+			result = insert_last(result, firstTemp->coef, firstTemp->expon);
+			firstTemp = firstTemp->next;
+
+		}
+		else {
+			result = insert_last(result, secondTemp->coef, secondTemp->expon);
+			secondTemp = secondTemp->next;
+		}
+
+
+	}
+	while (firstTemp != NULL) {
+		result = insert_last(result, firstTemp->coef, firstTemp->expon);
+		firstTemp = firstTemp->next;
+	}
+	while (secondTemp != NULL) {
+		result = insert_last(result, secondTemp->coef, secondTemp->expon);
+		secondTemp = secondTemp->next;
+	}
+
+
+	return result;
+
+}
+
+
+
+
+void printNode(ListNode *node) {
+
+	struct linkedNum *temp = node;
+	while (temp->next != NULL)
+	{
+		printf("%dX^%d+", temp->coef, temp->expon);
+		temp = temp->next;
+	}
+	if (temp != NULL) {
+		printf("%dX^%d", temp->coef, temp->expon);
+	}
+	printf("\n");
+}
+
+
+ListNode* reverse(ListNode *head) {
+	ListNode *q, *p, *r;
+	p = head;
+	q = NULL;
+	while (p != NULL) {
+		r = q;
+		q = p;
+		p = p->next;
+		q->next = r;
+	}
+	return q;
+}
+
+
+int main() {
+	ListNode *p;
+	p = (ListNode *)malloc(sizeof(ListNode));
+	p->coef = 3;
+	p->expon = 12;
+	p->next = NULL;
+//	printNode(p);
+
+	p = insert_last(p, 2, 8);
+	//printNode(p);
+	p = insert_last(p, 5, 3);
+	p = insert_last(p, 1, 0);
+	printNode(p);
+
+	ListNode *q;
+	q = NULL;
+	q = insert_last(q, 5, 12);
+	q = insert_last(q, 5, 6);
+	printNode(q);
+
+	ListNode *result = AddPoly(p, q);
+	printNode(result);
+
+
+}
