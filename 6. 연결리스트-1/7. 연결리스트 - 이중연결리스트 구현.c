ca66900e52eebe3a215c7fadd5cd1dcdf3b7d274
김도현 <39308953+wookoo@users.noreply.github.com>
2019-06-01 18:52:41 +0900

    삭제, 삽입 구현

--- 6. 연결리스트-1/7. 연결리스트 - 이중연결리스트 구현.c
+++ 6. 연결리스트-1/7. 연결리스트 - 이중연결리스트 구현.c
@@ -42,12 +42,47 @@ Node* delete_first(Node* node) {
 	return node;
 }
 
+Node* delete_node(Node* head, Node* prev) {
+	if (head == NULL || prev == NULL) {
+		return head;
+	}
+	Node* remove = prev->next;
+	prev->next = remove->next;
+	remove->next->prev = prev;
+	free(remove);
+	return head;
+	//Node* temp = target->next;
+	//temp->prev = target->prev;
+}
+
+Node* insert(Node* head, Node* prev, elemant data) {
+	if (head == NULL || prev == NULL) {
+		return head;
+	}
+	Node *create = (Node*)malloc(sizeof(Node));
+	create->next = prev->next;
+	create->prev = prev;
+	create->data = data;
+	prev->next = create;
+	return head;
+
+}
+
 int main() {
 	Node *head = NULL;
 	head = insert_last(head, 5);
 	head = insert_last(head, 9);
 	print_node(head);
 	head = delete_first(head);
+	print_node(head);
+	head = insert(head,head,6);
+	print_node(head);
+	head = insert(head, head, 3);
+	print_node(head);
+	head = insert(head, head->next, 1);
+	print_node(head);
+	head = delete_node(head, head);
+	
 	print_node(head);
 	return 0;
 }
