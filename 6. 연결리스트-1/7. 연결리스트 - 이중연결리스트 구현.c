dbab28f5ecd31ad5a251c4d8dfe89c5c65e8b1ad
김도현 <39308953+wookoo@users.noreply.github.com>
2019-06-12 04:20:48 +0900

    소스 수정

--- 6. 연결리스트-1/7. 연결리스트 - 이중연결리스트 구현.c
+++ 6. 연결리스트-1/7. 연결리스트 - 이중연결리스트 구현.c
@@ -1,89 +1,64 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
-typedef int elemant;
-typedef struct LinkedNum {
-	int data;
-	struct LinkedNum *prev;
-	struct LinkedNum *next;
-}Node;
 
-Node* insert_last(Node* node,elemant data){
-	Node *create = (Node *)malloc(sizeof(Node));
-	create->next = NULL;
-	create->prev = NULL;
-	create->data = data;
-	if (node == NULL) {
-		return create;
-	}
-	Node *last = node;
-	while (last->next != NULL) {
-		last = last->next;
-	}
-	last->next = create;
-	create->prev = last;
+typedef int element;
+typedef struct DList {
+	element data;
+	struct DList* rlink;
+	struct DList* llink;
+}DNode;
 
-	return node;
-}
-void print_node(Node* node) {
-	Node* temp = node;
-	while (temp != NULL) {
-		printf("%d->", temp->data);
-		temp = temp->next;
-	}
-	printf("NULL\n");
-}
-Node* delete_first(Node* node) {
-	if (node == NULL) {
-		return node;
-	}
-	Node* remove = node;
-	node = node->next;
-	free(remove);
-	return node;
+void init(DNode *node) {
+	node->llink = node->rlink = node;
 }
 
-Node* delete_node(Node* head, Node* prev) {
-	if (head == NULL || prev == NULL) {
-		return head;
-	}
-	Node* remove = prev->next;
-	prev->next = remove->next;
-	remove->next->prev = prev;
-	free(remove);
-	return head;
-	//Node* temp = target->next;
-	//temp->prev = target->prev;
-}
+void printNode(DNode *node) {
+	DNode *temp = node->rlink;
 
-Node* insert(Node* head, Node* prev, elemant data) {
-	if (head == NULL || prev == NULL) {
-		return head;
-	}
-	Node *create = (Node*)malloc(sizeof(Node));
-	create->next = prev->next;
-	create->prev = prev;
-	create->data = data;
-	prev->next = create;
-	create->next->prev = create;
-	return head;
-
-}
-
-int main() {
-	Node *head = NULL;
-	head = insert_last(head, 5);
-	head = insert_last(head, 9);
-	print_node(head);
-	head = delete_first(head);
-	print_node(head);
-	head = insert(head,head,6);
-	print_node(head);
-	head = insert(head, head, 3);
-	print_node(head);
-	head = insert(head, head->next, 1);
-	print_node(head);
-	head = delete_node(head, head);
+	while (temp != node) {
+		printf(" <-|%d|-> ", temp->data);
+		temp = temp->rlink;
 	
-	print_node(head);
-	return 0;
+	}
+/*
+	for (temp = temp->rlink; temp != node; temp = temp->rlink)
+	{
+		printf(" <-|%d|-> ", temp->data);
+
+
+	}*/
+	printf("\n");
+}
+void dinsert(DNode *node,element data) {
+	DNode *newNode = (DNode *)malloc(sizeof(DNode));
+
+	newNode->data = data;
+	newNode->llink = node;
+	newNode->rlink = node->rlink;
+	node->rlink->llink = newNode;
+	node->rlink = newNode;
+}
+void ddelete(DNode *head, DNode *target) {
+	if (target == head) {
+		return;
+	}
+	target->llink->rlink = target->rlink;
+	target->rlink->llink = target->llink;
+	free(target);
+}
+int main() {
+	DNode *newList = (DNode *)malloc(sizeof(DNode));
+	init(newList);
+	for (int i = 0; i < 10; i++) {
+		dinsert(newList, i);
+		printNode(newList);
+	}
+	for (int i = 0; i < 10; i++) {
+	
+		ddelete(newList, newList->rlink);
+		printNode(newList);
+
+	}
 }
